fad056954b23bad6ab67dd58a33924c03b4fc2f9
megaBossMike <101575455+megaBossMike@users.noreply.github.com>
2022-06-30 16:34:31 -0400

    Moved Tactics to start of round

--- src/factions/orruk_warclans/big_waaagh/battle_traits.ts
+++ src/factions/orruk_warclans/big_waaagh/battle_traits.ts
@@ -26,7 +26,7 @@ const BigWaaaghBattleTraits = {
       {
         name: `Wait For It, Ladz...`,
         desc: `You can pick this battle tactic only if your army has at least 24 Waaagh! points (pg 88). You complete this tactic if your army has at least 30 Waaagh! points at the end of this turn.`,
-        when: [START_OF_HERO_PHASE],
+        when: [START_OF_ROUND],
       },
     ],
   },
